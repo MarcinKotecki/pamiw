77d36ef26a6445584a4d8cc9c325db4434d57755
Marcin Kotecki <01142575@pw.edu.pl>
2020-11-22 22:05:19 +0100

    run script

--- rundocker.sh
+++ rundocker.sh
@@ -3,4 +3,5 @@ if ! grep -q "SECRET_KEY=" ".env"; then
     key="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)"
     echo -e "\nSECRET_KEY=${key}" >> .env
 fi
+docker-compose build
 docker-compose up
