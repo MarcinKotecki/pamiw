cd512bee0a8288d3f09603b8b6de244275480a96
Marcin Kotecki <01142575@pw.edu.pl>
2020-12-11 20:44:02 +0100

    milestone 3

---
+++ jwt_gen.py
@@ -0,0 +1,18 @@
+import jwt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+JWT_SECRET = os.environ.get('JWT_SECRET')
+
+payload = {
+        "iss": "paczkaplus",
+        "aud": "paczkaplus",
+        "iat": datetime.utcnow(),
+        "exp": datetime.utcnow() + timedelta(seconds=31536000),
+        "sub": "",
+        "usertype": "courier",
+    }
+token = jwt.encode(payload, JWT_SECRET, algorithm='HS256').decode()
+print(token)
